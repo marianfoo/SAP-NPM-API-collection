7bc10551da35ce950ee1045ccd051b53e4ac9849
„marianfoo“ <13335743+marianfoo@users.noreply.github.com>
2024-01-09 13:58:05 +0100

    update script

--- build.sh
+++ build.sh
@@ -5,10 +5,24 @@ cp new-package.json package.json
 git diff HEAD -- package.json | grep '^+\s*"' | cut -d '"' -f 2 > packages.txt
 
 while read package; do
-  ./npm_download.sh $package
-  packageNoPrefix=`echo $package | sed 's/@sap//g'`
-  mkdir "apis$packageNoPrefix"
-  rsync -zarv  --include "*/" --include="*.md" --exclude="*" "node_modules/$package/" "apis$packageNoPrefix"
-  cp node_modules/$package/LICENS* apis$packageNoPrefix
-  cp -r node_modules/$package/doc apis$packageNoPrefix/doc
+  if ./npm_download.sh "$package"; then
+    packageNoPrefix=$(echo $package | sed 's/@sap//g')
+    mkdir "apis$packageNoPrefix"
+
+    if ! rsync -zarv --include "*/" --include="*.md" --exclude="*" "node_modules/$package/" "apis$packageNoPrefix"; then
+      echo "Error in rsync for package $package"
+      continue
+    fi
+
+    if ! cp node_modules/$package/LICENS* "apis$packageNoPrefix"; then
+      echo "Error copying LICENSE for $package"
+      continue
+    fi
+
+    if ! cp -r node_modules/$package/doc "apis$packageNoPrefix/doc"; then
+      echo "Error copying documentation for $package"
+    fi
+  else
+    echo "Failed to download package $package"
+  fi
 done <packages.txt
